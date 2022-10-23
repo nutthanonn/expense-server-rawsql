9e82736cda82fba266d77453ec3377661029198f
nutthanon <nutthanon.tho@gmail.com>
2022-10-23 21:21:57 +0700

    [+] add: handler type

--- src/handlers/handlers.ts
+++ src/handlers/handlers.ts
@@ -4,8 +4,13 @@ import { createTransectionRouter } from "../routers/createTransectionRouter";
 import { fetchAllDataRouter } from "../routers/fetchAllDataRouter";
 import { deleteCardRouter } from "../routers/deleteCardRouter";
 import { fetchTransectionByIdRouter } from "../routers/fetchTransectionByIdRouter";
+import { IRouter } from "express";
 
-export const handlers = {
+interface Handler {
+  [key: string]: IRouter;
+}
+
+export const handlers: Handler = {
   createCardRouter,
   fetchCardRouter,
   createTransectionRouter,
